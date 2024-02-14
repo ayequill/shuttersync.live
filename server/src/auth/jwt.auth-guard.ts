503270d5df0f5822763b40b6806d66e0678aef8e
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-14 15:32:34 +0000

    Add JWT authentication and authorization functionality

---
+++ server/src/auth/jwt.auth-guard.ts
@@ -0,0 +1,47 @@
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+  ) {
+    super();
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.get('isPublic', context.getHandler());
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    const token = this.extractTokenFromHeader(request);
+    try {
+      this.jwtService.verifyAsync(token);
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid');
+    }
+  }
+
+  filter(exception: UnauthorizedException, context: ExecutionContext) {
+    return new UnauthorizedException(
+      'You are not authorized to access this resource',
+    );
+  }
+}
---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {AuthService} from "@/auth/auth.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.authService.validateUser(
+      payload?.email, payload?.password
+    )
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return user
+  }
+}
