6b03b7524d125da96b65e6bfe799cb79a19ddcd3
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-14 15:30:12 +0000

    Add user DTO and entity classes

---
+++ server/src/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+    @ApiProperty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    password: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+}
