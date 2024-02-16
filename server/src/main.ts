77451cca8e2d70acb9bd5a6dd8d9062621d87832
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-16 13:02:18 +0000

    Add cookie-parser middleware and enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,9 +12,13 @@ async function bootstrap() {
         .setVersion('1.0')
         .addTag('shuttersync')
         .build();
-
+     
+    app.enableCors();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
+    app.use(cookieParser(process.env.JWT_SECRET));
+
+
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
