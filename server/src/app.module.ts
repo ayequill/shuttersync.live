1ea998842d06aed09fb99657244a9f021624e669
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-16 13:07:11 +0000

    Add imports for AlbumsModule, PhotosModule, and CloudinaryModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,12 @@ import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt.auth-guard';
+import { AlbumsModule } from './albums/albums.module';
+import { PhotosModule } from './photos/photos.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, AlbumsModule, PhotosModule, CloudinaryModule],
   controllers: [],
   providers: [PrismaService, {
     provide: APP_GUARD,
