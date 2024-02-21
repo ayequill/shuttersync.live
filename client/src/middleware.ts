5128f349c3b7d2088692937c14357e0e4b70ac8b
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-21 03:02:22 +0000

    Add middleware for authentication

---
+++ client/src/middleware.ts
@@ -0,0 +1,34 @@
+
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  callbacks: {
+    authorized: async ({ req, token }) => {
+      const pathname = req.nextUrl.pathname;
+      // console.log(pathname);
+
+      // if 
+      // if (pathname.endsWith('/')){
+      //   return true;
+      // };
+  
+      // return true;
+      return token ? true : false;
+    },
+    redirect: async({token}) => {
+      return '/login';
+    }
+    // redirect: async ({ req, res, token }) => {
+    //   if (!token) {
+    //     res.writeHead(302, { Location: '/login' });
+    //     res.end();
+    //     return;
+    //   }
+    // }
+  },
+  secret: process.env.JWT_SECRET,
+});
+
+export const config = {
+  matcher: ['/dashboard'],
+};
