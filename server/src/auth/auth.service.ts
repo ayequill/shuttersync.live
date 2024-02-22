8ed3941cbb8f992703a477b93a2cc1b71cc11407
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-22 17:54:22 +0000

    Refactor user object in AuthService

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -47,7 +47,7 @@ export class AuthService {
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
-      id: user.id,
+      ...user
     };
   }
 }
