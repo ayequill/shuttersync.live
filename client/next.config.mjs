5946735fe1d3065c320bf797b44e877bf6215ed6
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-04-07 22:57:27 +0000

    Update Next.js config to include remotePatterns for image domains and remove webpack5 flag

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,10 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack5: true,
+    // webpack5: true,
     images: {
-        domains: ['res.cloudinary.com', 'images.unsplash.com', 'placehold.co'],
-        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                hostname: 'res.cloudinary.com',
+                protocol: 'https',
+            },
+            {
+                hostname: 'images.unsplash.com',
+                protocol: 'https',
+            },
+            {
+                hostname: 'placehold.co',
+                protocol: 'https',
+            },
+            {
+                hostname: 'ui-avatars.com',
+                protocol: 'https',
+            },
+            {
+                hostname: 'via.placeholder.com',
+                protocol: 'https',
+            },
+        ]
     },
     // async headers() {
     //     return [
