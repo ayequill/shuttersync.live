af0d08bce3b26c05cd1a68997162b85f891f1590
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-08-13 21:01:47 +0000

    modified authentication middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,57 +3,30 @@ import { getToken } from 'next-auth/jwt';
 
 const secret = process.env.NEXTAUTH_SECRET;
 
+const publicRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/reset-password",
+  "/welcome",
+  "/verify-email",
+  "email-verified",
+  "/",
+];
+
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret });
-  console.log(token);
-  // if (
-  //   token &&
-  //   (req.nextUrl.pathname === '/signin' ||
-  //     req.nextUrl.pathname === '/signup' ||
-  //     req.nextUrl.pathname === '/')
-  // ) {
-  //   return NextResponse.redirect(new URL('/dashboard', req.url));
-  // }
-  // if (
-  //   !token &&
-  //   req.nextUrl.pathname !== '/signin' &&
-  //   req.nextUrl.pathname !== '/signup' &&
-  //   req.nextUrl.pathname !== '/'
-  // ) {
-  //   return NextResponse.redirect(new URL('/signin', req.url));
-  // }
-
+  const isPublic = publicRoutes.some((route) =>
+    route.startsWith(req.nextUrl.pathname),
+  );
+  if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/signin", req.nextUrl).toString());
+  }
+  if (token && isPublic) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl).toString());
+  }
   return NextResponse.next();
 }
-
-// export default withAuth({
-//   callbacks: {
-//     authorized: async ({ req, token }) => {
-//       const pathname = req.nextUrl.pathname;
-//       // console.log(pathname);
-
-//       // if
-//       // if (pathname.endsWith('/')){
-//       //   return true;
-//       // };
-
-//       // return true;
-//       return token ? true : false;
-//     },
-//     // redirect: async ({ req, res, token }) => {
-//     //   if (!token) {
-//     //     res.writeHead(302, { Location: '/login' });
-//     //     res.end();
-//     //     return;
-//     //   }
-//     // }
-//   },
-//   secret: process.env.JWT_SECRET,
-// });
-
-// export const config = {
-//   matcher: ['/dashboard'],
-// };
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$|.*\\.webp$).*)'],
