05382ddfcbf39563485ced0f5a46afddf1eaecf8
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-08-13 21:04:02 +0000

    implemented session

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,7 +20,27 @@ async function bootstrap() {
   });
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.use(cookieParser());
+  app.use(cookieParser(process.env.JWT_SECRET));
+  app.use(
+    session({
+      secret: process.env.JWT_SECRET || 'secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+      },
+    }),
+  );
+  // app.use(
+  //   csurf({
+  //     cookie: {
+  //       secure: process.env.NODE_ENV === 'production',
+  //       sameSite: 'strict',
+  //     },
+  //   }),
+  // );
 
   await app.listen(process.env.PORT ?? 8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
